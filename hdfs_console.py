e0d977d21db50b6cd38b22063ae853468aa084c0
YunusovSamat <samat.yunusov@bk.ru>
2019-09-21 20:13:33 +0300

    Added put, get and append commands.

--- hdfs_console.py
+++ hdfs_console.py
@@ -13,7 +13,7 @@ class HDFSConsole:
         self._params = {'user.name': self._user, 'op': ''}
 
     def make_paths(self, path):
-        full_list_path = list() if path[0] == '/' else self._hdfs_path[:]
+        full_list_path = list() if os.path.isabs(path) else self._hdfs_path[:]
         list_path = tuple(filter(None, path.split('/')))
         for block in list_path:
             if block == '.':
@@ -46,8 +46,54 @@ class HDFSConsole:
                         print('\tNo created')
                 else:
                     print('\t', response.reason, sep='')
-        
-
+        elif op in ('put', 'append'):
+            if len(cmd_list_paths) != 2:
+                print('The number of arguments is not two')
+                return
+            if op == 'put':
+                self._params['op'] = 'CREATE'
+                op_str = 'Created'
+                method = 'PUT'
+                # self._params['permission'] = '777'
+            else:
+                self._params['op'] = 'APPEND'
+                op_str = 'Append'
+                method = 'POST'
+            if os.path.isfile(cmd_list_paths[0]):
+                file = open(cmd_list_paths[0], 'rb')
+                if not os.path.basename(cmd_list_paths[1]):
+                    cmd_list_paths[1] += os.path.basename(cmd_list_paths[0])
+                response, full_path = self.http_request(
+                    method, cmd_list_paths[1], data=file)
+                print(full_path, ':', sep='')
+                if response.reason in ('Created', 'OK'):
+                    print('\t', op_str, sep='')
+                else:
+                    print('\t', response.reason, sep='')
+                    print(response.text)
+            else:
+                print(cmd_list_paths[0],
+                      ':\n\tNot found or object not file', sep='')
+        elif op == 'get':
+            if len(cmd_list_paths) != 2:
+                print('The number of arguments is not two')
+                return
+            self._params['op'] = 'OPEN'
+            path_dir, path_name = os.path.split(cmd_list_paths[1])
+            if not os.path.isdir(path_dir):
+                print(cmd_list_paths[1], ':\n\tNot found', sep='')
+                return
+            elif not path_name:
+                cmd_list_paths[1] += os.path.basename(cmd_list_paths[0])
+            response, full_path = self.http_request(
+                'GET', cmd_list_paths[0])
+            if response.reason == 'OK':
+                print(cmd_list_paths[1], ':', sep='')
+                with open(cmd_list_paths[1], 'w') as file:
+                    file.write(response.text)
+                    print('\tWrited')
+            else:
+                print(full_path, ':\n\t', response.reason, sep='')
         elif op == 'delete':
             if not len(cmd_list_paths):
                 print('The number of arguments is less than one')
@@ -74,7 +120,7 @@ class HDFSConsole:
                     for file in response.json()['FileStatuses']['FileStatus']:
                         print('\t', file['pathSuffix'], sep='')
                 else:
-                    print(response.reason)
+                    print('\t', response.reason, sep='')
         elif op == 'cd':
             if len(cmd_list_paths) == 1:
                 self._params['op'] = 'GETFILESTATUS'
@@ -97,7 +143,7 @@ class HDFSConsole:
                     for block in os.listdir(path):
                         print('\t', block, sep='')
                 else:
-                    print('\tPath not found or object not directory')
+                    print('\tNot found or object not directory')
         elif op == 'lcd':
             if len(cmd_list_paths) == 1:
                 print(cmd_list_paths[0], ':', sep='')
@@ -107,11 +153,13 @@ class HDFSConsole:
                     print('\tPath not found or object not directory')
             else:
                 print('The number of arguments is not one')
+        elif op in ('exit', 'quit'):
+            exit(0)
         else:
             print('Command not found')
             return
 
 
 if __name__ == '__main__':
-    hdfs_console = HDFSConsole(user='samat')
-    hdfs_console.command_processing('put', ['psql_help.txt', 'text1.txt'])
+    hdfs_console = HDFSConsole()
+    hdfs_console.command_processing('get', [])
